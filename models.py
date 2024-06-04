b9e1ee26780bdf2143b10415132e23bee2705300
Ikramali <ikramaliambaliyasana2030@gmil.com>
2024-06-04 11:00:00 +0530

    change in model and route

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 # from sqlalchemy.sql.expression import null
 from database import Base
-from sqlalchemy import String, Integer, Column, Float, Date, ForeignKey
+from sqlalchemy import String, Integer, Column, Float, Date, ForeignKey, relationship
 
 class Payment(Base):
     __tablename__ = 'payment'
@@ -10,6 +10,4 @@ class Payment(Base):
     payment_date = Column(Date, nullable=False)
     payment_method = Column(String(50), nullable=False)
 
-
-    def __repr__(self):
-        return f"<Payment member_id={self.member_id} amount={self.amount} payment_date={self.payment_date} payment_method={self.payment_method}>"
+    member = relationship('Member', back_populates='payments')
